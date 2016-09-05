e939e340e72c7a30a0ca4c77cab1ba7fa3d3b664
Kamil Trzcinski <ayufan@ayufan.eu>
2016-09-05 15:02:30 +0200

    Add ATV framework

--- atv/atv-vendor.mk
+++ atv/atv-vendor.mk
@@ -32,7 +32,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/google/atv/misc/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+		vendor/google/atv/misc/com.google.android.pano.v1.jar:system/framework/com.google.android.pano.v1.jar \
     vendor/google/atv/misc/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar: \
     vendor/google/atv/misc/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+		vendor/google/atv/misc/com.google.android.pano.v1.xml:system/etc/permissions/com.google.android.pano.v1.xml \
     vendor/google/atv/misc/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
-
